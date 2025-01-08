9b09f556932cf5c12b548549be82f1f042008b30
aleksandar.blagoev <aleksandar.blagoev@delasport.com>
2025-01-08 11:11:07 +0200

    Changes on navigation and new components added

---
+++ vue-4-router/vue-travel-app/src/components/AppLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <a v-if="isExternal" :href="props.to" target="_blank"><slot /></a>
+  <router-link v-else v-bind="props"><slot /></router-link>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+import { computed } from 'vue'
+
+const props = defineProps({
+  ...RouterLink.props,
+})
+
+const isExternal = computed(() => {
+  return typeof props.to === 'string' && props.to.startsWith('http')
+})
+</script>
---
+++ vue-4-router/vue-travel-app/src/views/Protected.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Greeting, {{ username }}</h1>
+    <button @click="logout">Logout</button>
+    <AppLink :to="{ name: 'invoices' }">
+      <button>Invoices</button>
+    </AppLink>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+import AppLink from '@/components/AppLink.vue'
+
+const username = window.user
+const router = useRouter()
+
+const logout = () => {
+  window.user = null
+  router.push({ path: '/' })
+}
+</script>
